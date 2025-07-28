2ad95dfd71f0babcd5ee704f7c31370608315141
Samuel Victor Cotrim da Silva <samuelvictorcotrimdasilva@gmail.com>
2025-07-28 13:22:21 -0300

    Create manipueconver.c

---
+++ ModoETipodeDados.c/manipueconver.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    int b = 3;
+    int soma = a + b;
+    int diferença = a - b;
+    int produto = a * b;
+    int quociente = a / b; // Note que a divisão é inteira
+
+    printf("Soma: %d\n", soma);
+    printf("diferença: %d\n", diferença);
+    printf("produto: %d\n", produto);;
+    printf("Quociente: %d\n", quociente);
+
+    return 0;
+}
